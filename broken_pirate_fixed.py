5aca6d05a9d37d0d116afcfc4bccdb6eb24a559f
ryan <ryan170805@icloud.com>
2023-11-23 06:30:28 -0800

    first edit

---
+++ broken_pirate_fixed.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ("Arrr!"): #square brackets changed to parenthesis
+	print("Go away, pirate.")
+else: #add colon and elif changed to else
+    print("Greetings, hater of pirates!") #indentation
